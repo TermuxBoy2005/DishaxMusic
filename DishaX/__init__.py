fb01cb57b204ece1d7deeccc6152b287d4619d03
TermuxBoy2005 <130340748+TermuxBoy2005@users.noreply.github.com>
2023-04-12 23:16:45 +0530

    Create __init__.py

---
+++ DishaX/__init__.py
@@ -0,0 +1,37 @@
+from DishaX.core.bot import DishaXBot
+from DishaX.core.dir import dirr
+from DishaX.core.git import git
+from DishaX.core.userbot import Userbot
+from DishaX.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+
+dirr()
+
+git()
+
+dbb()
+
+heroku()
+
+sudo()
+
+# Clients
+app = DishaXBot()
+
+userbot = Userbot()
+
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
